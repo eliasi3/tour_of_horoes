d521b1a5a20b6f0264040f3c8ce33db5d8b42b0e
Elias <eliasi3solucoes@gmail.com>
2021-10-18 17:55:45 -0300

    commit de alteração de adição de tipos de campos na tabela heroes

--- db/migrate/20211014181348_create_heros.rb
+++ db/migrate/20211014181348_create_heros.rb
@@ -2,6 +2,8 @@ class CreateHeros < ActiveRecord::Migration[6.1]
   def change
     create_table :heros do |t|
       t.string :name
+      t.string :idade
+      t.string :personalidade
       t.timestamps
     end
   end
